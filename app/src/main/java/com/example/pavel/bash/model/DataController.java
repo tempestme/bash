84c19a340896be92874ec9b13c941d45aa4f33bd
tempestme <paulsecondtwin@gmail.com>
2018-01-16 01:44:56 +0300

    final fix of mergeArrayList() method

--- app/src/main/java/com/example/pavel/bash/model/DataController.java
+++ app/src/main/java/com/example/pavel/bash/model/DataController.java
@@ -1,8 +1,7 @@
 package com.example.pavel.bash.model;
 
-import android.util.Log;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.regex.Pattern;
 
 /**
@@ -13,24 +12,41 @@ public class DataController {
 
     public String returnClear(String rawText){
         String clearText = rawText;
-        clearText.replaceAll(Pattern.quote("<br />"),"");
-        clearText.replaceAll(Pattern.quote("&quot;"),"'");
-        clearText.replaceAll(Pattern.quote("&lt;"),"<");
-        clearText.replaceAll(Pattern.quote("&gt;"),">");
-        clearText.replaceAll(Pattern.quote("&laquo;"),"'");
-        clearText.replaceAll(Pattern.quote("&raquo;"),"'");
+        clearText = clearText.replaceAll(Pattern.quote("<br />"),"");
+        clearText = clearText.replaceAll(Pattern.quote("&quot;"),"'");
+        clearText = clearText.replaceAll(Pattern.quote("&lt;"),"<");
+        clearText = clearText.replaceAll(Pattern.quote("&gt;"),">");
+        clearText = clearText.replaceAll(Pattern.quote("&laquo;"),"'");
+        clearText = clearText.replaceAll(Pattern.quote("&raquo;"),"'");
+
 
         return clearText;
     }
 
     public void mergeArrayLists(ArrayList<Post> mainPosts, ArrayList<Post> newPosts){
-        ArrayList<Post> allPosts = new ArrayList<>();
-        newPosts.removeAll(mainPosts);  //todo:: something wrong with this method, newPosts.removeAll(mainPosts) doesn't work properly
-        Log.e("list","new posts size is: "+Integer.toString(newPosts.size()));
-        allPosts.addAll(newPosts);
-        //allPosts.addAll(mainPosts);
-        mainPosts.removeAll(mainPosts);
-        mainPosts.addAll(allPosts);
+        ArrayList<String> fine = new ArrayList<>();
+
+        for (int i=0;i<newPosts.size();i++){
+            fine.add(newPosts.get(i).getElementPureHtml());
+        }
+        for (int i=0;i<mainPosts.size();i++){
+            fine.add(mainPosts.get(i).getElementPureHtml());
+        }
+        mainPosts.clear();
+        HashSet<String> hs = new HashSet<>();
+        for (String string: fine
+             ) {    hs.add(string);
+
+        }
+        for (String string:hs
+             ) {
+            mainPosts.add(new Post(string));
+
+        }
+
+
 
     }
+
+
 }
--- app/src/main/java/com/example/pavel/bash/model/Post.java
+++ app/src/main/java/com/example/pavel/bash/model/Post.java
@@ -1,9 +1,5 @@
 package com.example.pavel.bash.model;
 
-import android.util.Log;
-
-import java.util.ArrayList;
-
 /**
  * Created by pavel on 02.01.18.
  */
@@ -43,35 +39,6 @@ public class Post {
     public String getElementPureHtml() {
         return elementPureHtml;
     }
-
-    /**
-     * @param mainList is the main list with all posts, it will be going straight to adapter.
-     * @param newPosts this will get all posts straight from internets. ->> addNewPosts() method will add this new posts to mainList
-     */
-    public void addNewPosts(ArrayList<Post> mainList, ArrayList<Post> newPosts){
-        Log.e("list", "mainList size is "+ Integer.toString(mainList.size()));
-        Log.e("list", "new posts size is "+ Integer.toString(newPosts.size()));
-        newPosts.removeAll(mainList);
-        Log.e("list","new posts after removal is "+ Integer.toString(newPosts.size()));
-        ArrayList<Post> allPosts = new ArrayList<Post>();
-        allPosts.addAll(newPosts);
-        Log.e("list","allpost size is "+ Integer.toString(allPosts.size()));
-        allPosts.addAll(mainList);
-        Log.e("list","allpost size is "+ Integer.toString(allPosts.size()));
-        mainList.clear();
-        mainList.addAll(allPosts);
-    }
-
-    /**
-     * method add ads to every fifth post
-     * @param posts is the main list of posts
-     * @return
-     */
-    public ArrayList<Post> addAds(ArrayList<Post> posts){
-
-        return posts;
-    }
-
 
 
 
